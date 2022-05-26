ead96e796c53df1b391649972b2cf7b784772642
VladisSer <vladisser2ex4@gmail.com>
2022-05-27 01:42:01 +0300

    Add desirebility field

--- Action.h
+++ Action.h
@@ -14,6 +14,7 @@ public:
     };
 
     Action();
+    Action(ActionType _actionType, int _executorId, int _againstId);
     PrefferedPoint getPrefferedPoint() const;
     bool operator==(const Action &rhs) const;
 
@@ -23,8 +24,7 @@ private:
     int againstId;
     PrefferedPoint prefferedPoint;
 
-
-
+    double desirebility;
 };
 
 #endif // ACTION_H
